291af0704b9cb0bcba7d951d1768844f82fc0a3c
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-05-16 13:49:09 -0600

    stub boot project

---
+++ CitySprouts/src/main/java/com/skilldistillery/midterm/CitySproutsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitySproutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitySproutsApplication.class, args);
+	}
+
+}
