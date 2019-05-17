13040febf0a66b0b500af4965cc71a353051422c
Randybeach <beach.randyj@gmail.com>
2019-05-17 10:53:22 -0600

    added logic to DAOImpl

--- CitySprouts/src/main/java/com/skilldistillery/midterm/data/SproutsDAO.java
+++ CitySprouts/src/main/java/com/skilldistillery/midterm/data/SproutsDAO.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.midterm.data;
 
+import java.util.List;
+
+import com.mysql.cj.jdbc.Driver;
+import com.skilldistillery.midterm.entities.Buyer;
+import com.skilldistillery.midterm.entities.Item;
+import com.skilldistillery.midterm.entities.Seller;
+
 public interface SproutsDAO {
 
+	public Driver getDriverById(int id);
+	public List<Driver> getAllDrivers();
+	public List<Seller> getAllSellers();
+	public List<Buyer> getAllBuyers();
+	public Seller getSellerById(int id);
+	public Buyer getBuyerById(int id);
+	public List<Item> getItemsByCategoryId(int id);
+	
 }
