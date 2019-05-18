72d02e205934efbea81ba035f5e6f6d879f29e46
Randybeach <beach.randyj@gmail.com>
2019-05-18 13:56:00 -0600

    changed view

--- CitySprouts/src/main/java/com/skilldistillery/midterm/controllers/ItemController.java
+++ CitySprouts/src/main/java/com/skilldistillery/midterm/controllers/ItemController.java
@@ -27,7 +27,7 @@ public class ItemController {
 		mv.addObject("random", randomList);
 		mv.addObject("fresh", sortedByDate);
 		
-		mv.setViewName("indexTest");
+		mv.setViewName("index");
 		return mv;
 	}
 }
