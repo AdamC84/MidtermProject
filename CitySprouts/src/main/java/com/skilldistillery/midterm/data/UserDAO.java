3fdd3f52efd6db8e4a2a00518cd54800431be0c3
Adam Crawford <adamcrawfordaf@yahoo.com>
2019-05-17 20:43:33 -0400

    UserDAO methods

--- CitySprouts/src/main/java/com/skilldistillery/midterm/data/UserDAO.java
+++ CitySprouts/src/main/java/com/skilldistillery/midterm/data/UserDAO.java
@@ -16,24 +16,37 @@ public interface UserDAO {
 	public User updateUser(int id, User u);
 	public User deleteUser(User u);
 	public List<User> getAllUsers();
+	public User getUserByEmail(String email);
+	public List<User> getUserByRole(String role);
+	public User getUserByUserName(String userName);
+	public List<User> getUserByKeyword(String keyword);
 	
 	public Driver getDriverById(int id);
 	public Driver addDriver(Driver d);
 	public Driver updateDriver(int id,Driver d);
 	public Driver deleteDriver(Driver d);
 	public List<Driver> getAllDrivers();
+	public Driver getDriverByBankAcctNum(String bankAcctNum);
+	public List<Driver> getDriverByBankName(String bankName);
+	public Driver getDriverByBankRouting(String bankRouting);
 	
 	public Buyer addBuyer(Buyer b);
 	public Buyer updateBuyer(int id, Buyer b);
 	public Buyer deleteBuyer(Buyer b);
 	public Buyer getBuyerById(int id);
 	public List<Buyer> getAllBuyers();
+	public Buyer getBuyerByBankRouting(String bankRouting);
+	public Buyer getBuyerByBankAcctNum(String bankAcctNum);
+	public List<Buyer> getBuyerByBankName(String bankName);
 	
 	public Seller addSeller(Seller s);
 	public Seller updateSeller(int id, Seller s);
 	public Seller deleteSeller(Seller s);
 	public Seller getSellerById(int id);
 	public List<Seller> getAllSellers();
+	public Seller getSellerByBankRouting(String bankRouting);
+	public Seller getSellerByBankAcctNum(String bankAcctNum);
+	public List<Seller> getSellerByBankName(String bankName);
 	
 	
 }
